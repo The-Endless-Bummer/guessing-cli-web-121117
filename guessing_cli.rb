020900d2b4ff370138f1632429a31cfcd868ecf9
Maurice Roy <mautayro@gmail.com>
2017-12-02 22:55:41 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,3 +1,18 @@
-def run_guessing_game
+def prompt
+  puts "Guess a number between 1 and 6."
+end
 
+def random
+  rand(1..6)
+end
+
+def exit
+
+end
+
+def run_guessing_game
+  prompt
+  input = gets.chomp
+  random
+  
 end
