86ec3d7e9fa8f814e14ca1643bc4354238f15e5a
Maurice Roy <mautayro@gmail.com>
2017-12-02 23:00:49 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,17 +2,18 @@ def prompt
   puts "Guess a number between 1 and 6."
 end
 
-def random
-  rand(1..6)
-end
-
 def exit
-
+  puts "Goodbye!"
 end
 
 def run_guessing_game
-  prompt
-  input = gets.chomp
-  random
-  
+  entry = nil
+  while entry != 'exit'
+    prompt
+    entry = gets.chomp
+  end
+  if [1, 2, 3, 4, 5, 6].include?(entry)
+    random = rand(1..6)
+    if entry == random
+  end
 end
